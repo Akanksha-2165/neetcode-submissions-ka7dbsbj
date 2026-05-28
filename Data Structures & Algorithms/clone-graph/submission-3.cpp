172e964c9bc1aacba315017776ed4a2e5d8a2c94
Akanksha-2165 <1nt23is017.akanksha@nmit.ac.in>
2026-05-28 22:00:59 +0530

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.cpp
@@ -0,0 +1,67 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    unordered_map<Node*,Node*> copies;
+
+    Node* cloneGraph(Node* node) {
+        if(!node)   return NULL;
+
+        if(copies.find(node)==copies.end()) {
+            copies[node]=new Node(node->val ,{});
+        
+            for(Node* neighbor :node->neighbors)  {
+                copies[node]->neighbors.push_back(cloneGraph(neighbor));
+            } 
+        }
+        return copies[node];
+    } 
+};
+
+/* BFS
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node)   return NULL;
+        unordered_map<Node*, Node*> copies;
+
+        Node* copy= new Node(node->val,{});
+        copies[node]=copy;
+
+        queue<Node*> todo;
+        todo.push(node);
+        while(!todo.empty())    {
+            Node* curr= todo.front();
+            todo.pop();
+            for(Node* neighbor: curr->neighbors)    {
+                if(copies.find(neighbor)==copies.end())  {
+                    copies[neighbor]=new Node(neighbor->val,{});
+                    todo.push(neighbor);
+                }
+                copies[curr]->neighbors.push_back(copies[neighbor]);
+            }
+        }
+        return copy;
+    }
+};
+*/
